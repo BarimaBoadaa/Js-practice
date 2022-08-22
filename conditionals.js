083ccc11033131fa88456d8660aa9ad971eb6c61
Samuel Boadaa <sboa24@gmail.com>
2022-08-22 12:59:59 +0000

    Initial commit

---
+++ conditionals.js
@@ -0,0 +1,7 @@
+function addNumbers(num1, num2, num3) {
+  let sum = num1 + num2 + num3;
+  console.log(sum);
+  return sum;
+}
+
+addNumbers(5, 6, 8);
---
+++ myscript.js
@@ -0,0 +1,13 @@
+let number1 = 55;
+let number2 = 10;
+let number3 = 2;
+
+let string1 = "Samuel ";
+let sring2 = "Boadaa";
+
+console.log(string1 + sring2);
+console.log(number1 * number2);
+console.log(number1 + number3);
+console.log(number1 - number2);
+console.log(number1 % number2);
+console.log(++number2);
